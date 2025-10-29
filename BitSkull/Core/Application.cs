ab9c13dd78a299d5d9a7538f84be8c4eba917432
insotheo <maxim.insotheo@gmail.com>
2025-10-29 22:06:05 +0400

    Application class

---
+++ BitSkull/Core/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BitSkull.Core
+{
+    public class Application
+    {
+        private static Application _inst;
+        private string _name;
+        public bool IsRunning { get; private set; } = false;
+
+        public Application(string appName)
+        {
+            if (_inst != null) throw new Exception("Application is already created!");
+
+            _name = appName;
+            _inst = this;
+        }
+
+        public static Application GetAppInstance() => _inst;
+
+        public void Run()
+        {
+            IsRunning = true;
+            while (IsRunning) ;
+            IsRunning = false;
+        }
+    }
+}
--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,6 @@
-﻿using BitSkull;
+﻿using System;
+using BitSkull;
+using BitSkull.Core;
 
 namespace Sandbox
 {
@@ -6,16 +8,16 @@ namespace Sandbox
     {
         static void Main()
         {
-            Log.Pattern = "[%T]: %e";
-
-            string[] skeleton = "\r\n               _.---._\r\n             .'       `.\r\n             :)       (:\r\n             \\ (@) (@) /\r\n              \\   A   /\r\n               )     (\r\n               \\\"\"\"\"\"/\r\n                `._.'\r\n                 .=.\r\n         .---._.-.=.-._.---.\r\n        / ':-(_.-: :-._)-:` \\\r\n       / /' (__.-: :-.__) `\\ \\\r\n      / /  (___.-` '-.___)  \\ \\\r\n     / /   (___.-'^`-.___)   \\ \\\r\n    / /    (___.-'=`-.___)    \\ \\\r\n   / /     (____.'=`.____)     \\ \\\r\n  / /       (___.'=`.___)       \\ \\\r\n (_.;       `---'.=.`---'       ;._)\r\n ;||        __  _.=._  __        ||;\r\n ;||       (  `.-.=.-.'  )       ||;\r\n ;||       \\    `.=.'    /       ||;\r\n ;||        \\    .=.    /        ||;\r\n ;||       .-`.`-._.-'.'-.       ||;\r\n.:::\\      ( ,): O O :(, )      /:::.\r\n|||| `     / /'`--'--'`\\ \\     ' ||||\r\n''''      / /           \\ \\      ''''\r\n         / /             \\ \\\r\n        / /               \\ \\\r\n       / /                 \\ \\\r\n      / /                   \\ \\\r\n     / /                     \\ \\\r\n    /.'                       `.\\\r\n   (_)'                       `(_)\r\n    \\\\.                       .//\r\n     \\\\.                     .//\r\n      \\\\.                   .//\r\n       \\\\.                 .//\r\n        \\\\.               .//\r\n         \\\\.             .//\r\n          \\\\.           .//\r\n          ///)         (\\\\\\\r\n        ,///'           `\\\\\\,\r\n       ///'               `\\\\\\\r\n      \"\"'                   '\"\"\r\n       Skeleton\r\n\r\n".Split("\n");
-
-            for(int i = 0; i < skeleton.Length; i++)
+            Log.Pattern = "[%T]: %E";
+            try
             {
-                if (i % 4 == 0) Log.Error(skeleton[i]);
-                else if (i % 3 == 0) Log.Warn(skeleton[i]);
-                else if (i % 2 == 0) Log.Info(skeleton[i]);
-                else Log.Trace(skeleton[i]);
+                Application sandboxApp = new Application("Sandbox");
+                //Application newApp = new("ABC"); //ERROR
+                sandboxApp.Run();
+            }
+            catch(Exception ex)
+            {
+                Log.Error(ex);
             }
         }
     }
